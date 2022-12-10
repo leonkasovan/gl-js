fcb6fa529719d30e941ccf7469805240d5a06d5b
leonkasovan <110372618+leonkasovan@users.noreply.github.com>
2022-12-10 11:35:13 +0700

    Support arm arch in linux

--- gl_opengles.go
+++ gl_opengles.go
@@ -2,8 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build (ios || android || linux) && (js || arm || arm64)
 // +build ios android linux
-// +build arm arm64
+// +build js arm arm64
 
 package gl
 
